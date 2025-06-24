33a8af6b0d9dcd88aeb111f462493ff3341496dd
Lukas Grünberger <s2310456003@fhooe.at>
2025-06-24 17:48:13 +0200

    serviceworkers pt1

---
+++ serviceworker.js
@@ -0,0 +1,9 @@
+self.addEventListener('fetch', function(event) {
+    console.log('Fetch!', event.request);
+});
+self.addEventListener("install", function(event) {
+    console.log("install");
+});
+self.addEventListener("activate", function(event) {
+    console.log("activate");
+});
