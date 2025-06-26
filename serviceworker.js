19947193f89d7d300d26d25a935d03fa91e3ed4b
Lukas Grünberger <s2310456003@fhooe.at>
2025-06-26 10:37:57 +0200

    stand Übung

--- serviceworker.js
+++ serviceworker.js
@@ -1,13 +1,48 @@
-self.addEventListener('fetch', function(event) {
-    console.log('Fetch!', event.request.url);
+const CACHE_NAME = "kwm-cache";
+const CACHED_URLS = [
+    "CSS/materialize.min.css",
+    "CSS/styles.css",
+    "IMGs/Icons/icon-144.png",
+    "JS/app.js",
+    "JS/materialize.min.js",
+    "index.html",
+    "/",
+    "manifest.webmanifest",
+];
+
+self.addEventListener("install", function(event) {
+    console.log("Service Worker installing.");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(c) {
+            return c.addAll(CACHED_URLS);
+        }).catch((err)=>{
+            console.error(err);
+        })
+    );
+})
+
+// Network first, falling back to cache strategy
+self.addEventListener("fetch", function(event) {
     event.respondWith(
-        fetch(event.request)
+        fetch(event.request).catch(function() {
+            return caches.match(event.request).then(function(response) {
+                return response;
+            });
+        })
     );
 });
 
-self.addEventListener("install", function(event) {
-    console.log("install");
-});
-self.addEventListener("activate", function(event) {
-    console.log("activate");
+self.addEventListener("activate",(event)=>{
+    event.waitUntil(
+        caches.keys().then((cacheNames)=>{
+            return Promise.all(
+                cacheNames.map((cacheName)=>{
+                    if(CACHE_NAME !== cacheName && cacheName.startsWith("kwm-cache")){
+                        console.log("Deleting old cache:", cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
