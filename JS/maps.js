de3c867e9e1957ed9fe1d53e174b9adac4d7c8e1
Lukas Grünberger <s2310456003@fhooe.at>
2025-07-07 20:42:57 +0200

    maps

--- JS/maps.js
+++ JS/maps.js
@@ -3,10 +3,4 @@ window.buildMapsUrl = async function(venue) {
     if (venue.location?.latitude && venue.location?.longitude) {
         return `https://www.google.com/maps/search/?api=1&query=${venue.location.latitude},${venue.location.longitude}`;
     }
-    const address = encodeURIComponent(
-        [venue.name, venue.city?.name]
-            .filter(Boolean)
-            .join(", ")
-    );
-    return `https://www.google.com/maps/search/?api=1&query=${address}`;
 };
